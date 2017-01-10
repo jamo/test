5582d9157bc696910bab51ab14b3292b414546ec
Jarmo Isotalo <jamo@isotalo.fi>
2017-01-10 22:48:05 +0200

    publish

--- scripts/prebublish.sh
+++ scripts/prebublish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
   echo "Deploying"
+  cat package.json |jq '.version = .version + "-" + $SHA' --arg SHA `git rev-parse --short=5 HEAD` > package.json
+  npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
+  npm publish --tag next
 else
   echo "Not on master or PR -> skipping deploy"
 fi
