2a67110859e3fbfd5287e3964978b40945a723a2
Jarmo Isotalo <jamo@isotalo.fi>
2017-01-10 23:03:10 +0200

    Fix

--- scripts/prebublish.sh
+++ scripts/prebublish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
   echo "Deploying"
-  cat package.json |jq '.version = .version + "-" + $SHA | .name = "nottest-nottest-nottest-nottest-nottest-nottest-nottest" ' --arg SHA `git rev-parse --short=5 HEAD`
+  cat package.json |jq '.version = .version + "-" + $SHA | .name = "nottest-nottest-nottest-nottest-nottest-nottest-nottest" ' --arg SHA `git rev-parse --short=5 HEAD` > package.json
   npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
   npm publish --tag next
 else
